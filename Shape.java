6761527809a041d8e47af747bb1ee000b98cdab2
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2023-09-05 19:10:33 +0300

    Java-3_3-5

---
+++ Shape.java
@@ -0,0 +1,4 @@
+abstract public class Shape {
+    abstract void Area();
+    abstract void Perimeter();
+}
---
+++ Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle extends Shape {
+
+    @Override
+    void Area() {
+
+    }
+
+    @Override
+    void Perimeter() {
+
+    }
+}
